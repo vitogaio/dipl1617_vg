9597362d57075946ee7c90933cdfa5a91113672b
vg_MBP <>
2017-08-13 22:29:54 +0200

    adding gulpfile/gitignor/slickslider

---
+++ js/slick.js
@@ -0,0 +1,11 @@
+/**
+ * Created by vitogaio on 12.08.17.
+ */
+$(document).ready(function(){
+    $('.slick').slick({
+        autoplay: true,
+        mobileFirst: false,
+        adaptiveHeight: true,
+        dots: false
+    });
+});
